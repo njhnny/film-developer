8fbef5a021cb01ba0d20a3bb5ae34697c21f0a3c
Nick Hennessy <njhnny@gmail.com>
2022-03-31 19:51:24 -0700

    separate site into component

---
+++ src/components/Site.js
@@ -0,0 +1,28 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+function Site() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Now we're making something!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Site;
