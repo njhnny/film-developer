afd0e357317101065fc64c6521f32e6f7fe6fb5b
Nick Hennessy <njhnny@gmail.com>
2022-04-03 20:04:41 -0700

    update to pass test

--- src/reducers/instructions-reducer.js
+++ src/reducers/instructions-reducer.js
@@ -1,9 +1,18 @@
-const defaultState = {
+import * as c from './../actions/ActionTypes';
+
+let initialState = {
   isLoading: false,
   instructions: [],
   error: null
 }
 
-export default (state = defaultState, action) => {
-  return state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case c.REQUEST_INSTRUCTIONS:
+      return Object.assign({}, state, {
+        isLoading: true
+      });
+    default:
+      return state;
+    }
 };
